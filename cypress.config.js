80ddb6bbf8eda58237f247e9eb30f293847156b4
kadi490 <82709655+kadi490@users.noreply.github.com>
2023-07-01 10:00:37 +0300

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const {defineConfig} = require("cypress");
+
+module.exports = defineConfig({
+    pageLoadTimeout: 16000,
+    chromeWebSecurity: false,
+
+    env: {
+        firstCookieValue: "firstValue",
+    },
+
+    e2e: {
+        setupNodeEvents(on, config) {
+            return config;
+        }
+    },
+});
